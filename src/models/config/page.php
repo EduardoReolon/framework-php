69c073044a1d1b08d95d71194889a36a22561786
Eduardo <eduardo.cohapar@hotmail.com>
2024-08-13 14:00:16 -0300

    adjustments

--- src/models/config/page.php
+++ src/models/config/page.php
@@ -40,27 +40,27 @@ class Page {
             <div aria-label="Page navigation example">
                 <ul class="pagination">
                     <?php
-                        echo "<li class=\"page-item\"><a class=\"page-link\" href=\"" . $this->linkPage(1, $query_string) . "\">Primeira</a></li>";
+                        echo "<li class=\"page-item\"><a add-hash class=\"page-link\" href=\"" . $this->linkPage(1, $query_string) . "\">Primeira</a></li>";
                         $pDisabled = $this->page > 1 ? '' : ' disabled';
-                        echo "<li class=\"page-item{$pDisabled}\"><a{$pDisabled} class=\"page-link\" href=\"" . $this->linkPage($this->page - 1, $query_string) . "\">Anterior</a></li>";
+                        echo "<li class=\"page-item{$pDisabled}\"><a add-hash {$pDisabled} class=\"page-link\" href=\"" . $this->linkPage($this->page - 1, $query_string) . "\">Anterior</a></li>";
 
                         $half_pages_visible = (int) floor($pages_visible / 2);
                         $start_page = (int) max($this->page - $half_pages_visible, 1);
                         $end_page = (int) min($start_page + $pages_visible - 1, $this->total_pages);
                         $start_page = (int) max($end_page - $pages_visible + 1, 1);
 
-                        if ($start_page > 1) echo "<li class=\"page-item disabled\"><a disabled class=\"page-link\" href=\"\">...</a></li>";
+                        if ($start_page > 1) echo "<li class=\"page-item disabled\"><a add-hash disabled class=\"page-link\" href=\"\">...</a></li>";
                         for ($i=$start_page; $i <= $end_page ; $i++) { 
                             $selected = $i === $this->page ? ' active' : '';
-                            echo "<li class=\"page-item{$selected}\"><a class=\"page-link\" href=\"{$this->linkPage($i, $query_string)}\">{$i}</a></li>";
+                            echo "<li class=\"page-item{$selected}\"><a add-hash class=\"page-link\" href=\"{$this->linkPage($i, $query_string)}\">{$i}</a></li>";
                         }
 
-                        if ($end_page < $this->total_pages) echo "<li class=\"page-item disabled\"><a disabled class=\"page-link\" href=\"\">...</a></li>";
+                        if ($end_page < $this->total_pages) echo "<li class=\"page-item disabled\"><a add-hash disabled class=\"page-link\" href=\"\">...</a></li>";
 
                         $nDisabled = $this->page < $this->total_pages ? '' : ' disabled';
-                        echo "<li class=\"page-item{$nDisabled}\"><a{$nDisabled} class=\"page-link\" href=\"" . $this->linkPage($this->page + 1, $query_string) . "\">Próximo</a></li>";
+                        echo "<li class=\"page-item{$nDisabled}\"><a add-hash {$nDisabled} class=\"page-link\" href=\"" . $this->linkPage($this->page + 1, $query_string) . "\">Próximo</a></li>";
                         
-                        echo "<li class=\"page-item\"><a class=\"page-link\" href=\"" . $this->linkPage($this->total_pages, $query_string) . "\">Última</a></li>";
+                        echo "<li class=\"page-item\"><a add-hash class=\"page-link\" href=\"" . $this->linkPage($this->total_pages, $query_string) . "\">Última</a></li>";
                     ?>
                 </ul>
             </div>
